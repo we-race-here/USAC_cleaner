7e87a9712e99cf855de3ecf10a2b2943fe1ac8d5
Vincent Davis <vincent@heteroskedastic.net>
2022-11-08 19:08:29 -0700

    making testing data utilities

---
+++ config.py
@@ -0,0 +1,7 @@
+
+defaultUSACfields = ['First Name', 'Last Name', 'Birthdate', 'Race Age', 'Race Gender', 'Sex', 'State', 'Zip',
+                     'License #', 'License Type', 'License Status', 'License Expiration', 'License Categories',
+                     'UCI Category', 'Club', 'Team', 'Event ID(s)', 'Event Name(s)', 'Event Date(s)',
+                     'Local Association', 'Racing Nationality', 'Citizenship Country', 'Citizenship Verified',
+                     'Emergency Contact Name', 'Emergency Contact Number', 'RD Cat', 'TR Cat', 'MB Cat', 'BX Cat',
+                     'CX Cat', 'BF Cat', 'MX Cat', 'DH Cat', 'XC Cat', 'MR Cat']
--- generatefake.py
+++ generatefake.py
@@ -1,25 +1,69 @@
-from faker import Faker
-from typing import Literal
-import random
 import csv
+import datetime
 import json
+import random
+from typing import Literal
 
-def generate(rows: int = 50, export: Literal['csv', 'json'] = 'csv', filename: str = 'clean'):
+from faker import Faker
+
+
+def generate_data(rows: int = 50, export: Literal['csv', 'json'] = 'csv', filename: str = 'valid_data'):
     """Generates fake registration or member data
     exports as csv or json"""
     fake = Faker()
     data = []
+    year = datetime.datetime.now().year
     for n in range(rows):
-        per = {license:random.randint(1000, 9999), 'first': fake.first_name(), 'last': fake.last_name(), 'email': fake.email(),
+        per = {'license': random.randint(1000, 9999), 'First Name': fake.first_name(), 'Last Name': fake.last_name(),
+               'email': fake.email(),
                'phone': fake.phone_number(), 'address': fake.address(), 'city': fake.city(), 'state': fake.state(),
                'zip': fake.zipcode(), 'country': fake.country(), 'gender': random.choice(["M", "F"]),
+               'Birthdate': fake.date_of_birth(),
                'category': random.choice(['CAT5', 'CAT3', 'MM60'])}
+        per.update({'Race Age': year - per['Birthdate'].year, 'Birthdate': per['Birthdate'].strftime('%d/%m/%Y')})
         data.append(per)
-    if export=='csv':
-        with open(f"{filename}.csv", 'w') as csvfile:
+    if export == 'csv':
+        with open(f"testdata/{filename}.csv", 'w') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=data[0].keys())
             writer.writeheader()
             writer.writerows(data)
-    elif export=='json':
-        with open(f"{filename}.json", "w") as final:
+    elif export == 'json':
+        with open(f"testdata/{filename}.json", "w") as final:
             json.dump(data, final)
+
+
+def currupt_data(filename: str = 'valid_data', export: Literal['csv', 'json'] = 'csv'):
+    """Takes a csv or json file and currupts it"""
+    fake = Faker()
+    fields = {'license': random.randint(1000, 9999), 'First Name': fake.first_name(), 'Last Name': fake.last_name(),
+              'email': fake.email(),
+              'phone': fake.phone_number(), 'address': fake.address(), 'city': fake.city(), 'state': fake.state(),
+              'zip': fake.zipcode(), 'country': fake.country(), 'gender': random.choice(["M", "F"]),
+              'Birthdate': fake.date_of_birth().strftime('%d/%m/%Y'),
+              'category': random.choice(['CAT5', 'CAT3', 'MM60']),
+              'Race_Age': random.randint(1, 100)}
+    if export == 'csv':
+        with open(f"testdata/{filename}.csv", 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            data = [row for row in reader]
+    else:
+        with open(f"testdata/{filename}.json", "r") as final:
+            data = json.load(final)
+    currupt = []
+
+    for row in data:
+        # print(f'currupting: {row}')
+        if random.randint(0, 100) < 50:  # 50% chance of curruption
+            random_field = random.choice(list(row.keys()))
+            row[random_field] = '' if random.randint(0, 100) < 20 else fields[random_field]
+        currupt.append(row)
+
+    if export == 'csv':
+        with open(f"testdata/{filename}_currupted.csv", 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=data[0].keys())
+            writer.writeheader()
+            writer.writerows(currupt)
+
+if __name__ == '__main__':
+    generate_data()
+    currupt_data()
